3b585530d0e27bde410e732d653c6abfd9fef433
JyLIU-emma <bfsufyxljy@163.com>
2021-05-27 11:59:18 +0200

    modify merge_s2s_ofcors

--- OFCORS/merge_s2s_ofcors.py
+++ OFCORS/merge_s2s_ofcors.py
@@ -8,8 +8,9 @@ exemple: (on a blabla.txt et blabla.cupt dans ./blabla)
 python merge_s2s_ofcors.py ./blabla/ blabla ./blabla/blabla.cupt
 """
 import sys
-from CuptParser import Cupt
-from OfcorsFilesParser import Mentions, CorefChaines, OfcorsOutput
+# from CuptParser import Cupt
+# from OfcorsFilesParser import Mentions, CorefChaines, OfcorsOutput
+from CuptParser import merge_cupt_ofcors
 
 rep_corpus = sys.argv[1]
 filename = sys.argv[2]
@@ -22,11 +23,16 @@ mention_file = f'{ofcors_rep}{filename}_mentions_output.json'
 coref_file = f'{ofcors_rep}{filename}_resulting_chains.json'
 token_file = f'{ofcors_rep}{filename}_tokens.json'
 
-mentions = Mentions(mention_file)
-coref = CorefChaines(coref_file)
-mentions.chainer(coref.ment_cluster)
-ofcors_out = OfcorsOutput(token_file)
-ofcors_out.merge_result(mentions)
-cupt = Cupt(cupt_file)
-cupt.add_ofcors_output(ofcors_out)
+##OLD
+# mentions = Mentions(mention_file)
+# coref = CorefChaines(coref_file)
+# mentions.chainer(coref.ment_cluster)
+# ofcors_out = OfcorsOutput(token_file)
+# ofcors_out.merge_result(mentions)
+# cupt = Cupt(cupt_file)
+# cupt.add_ofcors_output(ofcors_out)
+
+##NEW
+cupt = merge_cupt_ofcors(cupt_file, token_file, mention_file, coref_file)
+
 cupt.write_to_file(f'{output_rep}{filename}_mwe_coref.cupt')
