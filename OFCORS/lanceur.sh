ed39d2b04cef5c7bd597c060ca186b50a28982bb
anaelle-p <anaellepierredon@gmail.com>
2021-07-02 12:08:28 +0200

    nettoyage

--- OFCORS/lanceur.sh
+++ OFCORS/lanceur.sh
@@ -13,7 +13,7 @@ ofcors()
         do
             echo "--------------------------------------"
             echo $fichier
-            ofcors-infer -f -k stanza -p window --window-size 5 $fichier
+            ofcors-infer -f -k stanza -p window --window-size 8 $fichier
 
             new_fichier=(${fichier//\// })
             dernier=$((${#new_fichier[@]}-1))
